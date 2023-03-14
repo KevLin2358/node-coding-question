245e81d7f80fa20bb614fc7cdf4d154c4a704a47
KevLin2358 <klin2358@gmail.com>
2023-03-14 15:17:42 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const {Sequelize, DataTypes} = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
