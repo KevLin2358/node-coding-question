6d5870797ce22b4e75e0d4a3a8f170f6f024d4c2
KevLin2358 <klin2358@gmail.com>
2023-03-16 15:21:44 -0400

    updated passport

--- config/passport.js
+++ config/passport.js
@@ -5,22 +5,22 @@ const keys = require("./keys");
 const User = db.user;
 
 const options = {};
-options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // built in method for passport-jwt
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 
 options.secretOrKey = keys.secretOrKey;
 module.exports = (passport) => {
   passport.use(
     new JwtStrategy(options, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
+      User.findByPk(jwt_payload.id)
         .then((user) => {
           if (user) {
-            // return the user to the frontend
             return done(null, user);
           }
-          // return false since there is no user
           return done(null, false);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => 
+          console.log(err)
+        );
     })
   );
 };
