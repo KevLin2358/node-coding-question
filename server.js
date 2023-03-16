0f866a786e12891585cac278ac008097aa921019
KevLin2358 <klin2358@gmail.com>
2023-03-15 21:16:33 -0400

    validations for login and signup

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const db = require('./models');
-const userRoutes = require('./routes/userRoutes');
-
+const userRoutes = require('./routes/user.routes');
 const PORT = process.env.PORT || 8080;
-
 const app = express();
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());  
 app.use(cookieParser());
 
 db.sequelize.sync({force: true}).then(() => {
